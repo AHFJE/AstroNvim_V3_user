a8735cfa2f9bc7c4f1a1a0bd62b79fd2d3c15c79
AHFJE <hemiaoyang@foxmail.com>
2023-09-10 14:51:18 +0800

    update

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,56 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  -- { import = "astrocommunity.code-runner.sniprun" },
+  { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
+  -- { import = "astrocommunity.utility.neodim" },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        mode = "buffers",
+        numbers = "none",
+        diagnostics = "nvim_lsp",
+        diagnostics_update_in_insert = false,
+        -- indicator = {
+        --   style = "underline",
+        -- },
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "AHFJE",
+            text_align = "center",
+            separator = true,
+          },
+        },
+        separator_style = "slant",
+        color_icons = true,
+        hover = {
+          enabled = true,
+          delay = 200,
+          reveal = { "close" },
+        },
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      lsp = {
+        signature = {
+          enabled = false,
+        },
+      },
+    },
+  },
+
+  -- { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  -- {
+  --   "m4xshen/smartcolumn.nvim",
+  --   opts = {
+  --     colorcolumn = 120,
+  --     disabled_filetypes = { "help" },
+  --   },
+  -- },
 }
--- plugins/undotree.lua
+++ plugins/undotree.lua
@@ -5,7 +5,7 @@ return {
   },
   config = function()
     require("undotree").setup {
-      float_diff = true, -- using float window previews diff, set this `true` will disable layout option
+      float_diff = true,      -- using float window previews diff, set this `true` will disable layout option
       layout = "left_bottom", -- "left_bottom", "left_left_bottom"
       ignore_filetype = {
         "Undotree",
